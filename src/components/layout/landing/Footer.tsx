bd909c901a374d5bcfb110086ebbf391c09bce3d
Juan Prieto <jp1739@gmail.com>
2025-05-28 08:36:13 -0500

    feat:  update footer

--- src/components/layout/landing/Footer.tsx
+++ src/components/layout/landing/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <div className="footer-content">
       <p className="has-text-centered has-text-weight-semibold pt-2 pb-2">
-        WiseMed 2025 &copy; - Reservados todos los derechos.
+        Desarrollado por Juan Prieto
       </p>
     </div>
   );
